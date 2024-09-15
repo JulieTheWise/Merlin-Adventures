d447cbdf676481e8d998b479169e0f8f4301c42e
JulieTheWise <souperjaysserver@gmail.com>
2024-09-14 19:38:08 -0700

    first commit

---
+++ js/chat.js
@@ -0,0 +1,4 @@
+const chatViewElement = document.getElementById(chatView)
+
+var playerSays = new Element(<p class="PCChat"><i>YESSSSSS it worked!!!!</i></p>);
+chatViewElement.appendChild(playerSays)
